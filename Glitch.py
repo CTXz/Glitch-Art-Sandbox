ed9774c125a9d6a0d70a729cd8b6cc32fb88b6ed
Patrick Pedersen <ctx.xda@gmail.com>
2018-04-19 18:32:50 +0200

    License: Fix year
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- Glitch.py
+++ Glitch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Patrick Pedersen <ctx.xda@gmail.com>
+# Copyright (c) 2018 Patrick Pedersen <ctx.xda@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
